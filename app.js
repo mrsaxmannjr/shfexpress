631a1225fdd7a6cd013562d4ff5c58e5a4ec0392
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-03-20 09:32:24 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Yo",
+  });
+});
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  res.status(404);
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: req.app.get("env") === "development" ? err.stack : {},
+  });
+});
+
+module.exports = app;
